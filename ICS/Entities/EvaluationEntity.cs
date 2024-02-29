af05eced67a76a2a9e6c3c2f27aabcaeb9487409
tykatsheesh <tykatuwu@gmail.com>
2024-02-29 14:17:14 +0100

    added EvaluationEntity

---
+++ ICS/Entities/EvaluationEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace ICS.DAL.Entities;
+public record EvaluationEntity
+{
+    public required Guid ActivityId { get; set; }
+    public required Guid StudentId { get; set; }
+    public string? Description { get; set; }
+    public required int Points { get; set; }
+}
