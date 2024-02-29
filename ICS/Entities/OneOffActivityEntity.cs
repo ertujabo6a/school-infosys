5521ec98af1f7210c47466998936d80bacea72ba
Nikita Kotvitskiy <nikita.kotvitski@gmail.com>
2024-02-29 11:41:22 +0100

    feat: OneOff activity was implemented
    * OneOffActvityEntity contains implementation of OneOff activity entitiy
      with all its parameters

---
+++ ICS/Entities/OneOffActivityEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace ICS.DAL.Entities;
+public record OneOffActivityEntity : IActivity
+{
+    public DateTime ActivityDate { get; set; }
+
+    public Guid EntityId { get; set; }
+    public string SubjectAbbr { get; set; } = string.Empty;
+    public string ActivityName { get; set; } = string.Empty;
+    public string TeacherLogin { get; set; } = string.Empty;
+    public int Capacity { get; set; }
+    public int Registered { get; set; }
+    public string RoomName { get; set; } = string.Empty;
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public string Description { get; set; } = string.Empty;
+}
