2dadb09a2626a303291b6cf3c2a37f69ce141f15
Nikita Kotvitskiy <nikita.kotvitski@gmail.com>
2024-02-29 11:16:17 +0100

    feat: Activity entity
    * Activity.cs contains interface for abstract Activity entitiy with all
      its attributes

---
+++ ICS/Entities/Activity.cs
@@ -0,0 +1,15 @@
+﻿namespace ICS.DAL.Entities;
+
+public interface IActivity
+{
+    public Guid EntityId { get; set; }
+    public string SubjectAbbr { get; set; }
+    public string ActivityName { get; set; }
+    public string TeacherLogin { get; set; }
+    public int Capacity { get; set; }
+    public int Registered { get; set; }
+    public string RoomName { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public string Description { get; set; }
+}
