3afea2c3a6f2389f72aee7efc68b68a21d141cac
Andrii Bondarenko <xbonda06@vutbr.cz>
2024-02-29 18:17:57 +0000

    fix: protected to public visibility changed

--- ICS/Entities/User.cs
+++ ICS/Entities/User.cs
@@ -1,10 +1,10 @@
 ﻿namespace ICS.DAL.Entities;
 public abstract record User
 {
-    protected string Login { get; set; } = string.Empty;
-    protected string Password { get; set; } = string.Empty;
-    protected string FirstName { get; set; } = string.Empty;
-    protected string LastName { get; set; } = string.Empty;
+    public string Login { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
     public DateTime Birth { get; set; }
     public string PhotoUrl { get; set; } = string.Empty;
 
