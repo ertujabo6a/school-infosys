5ea9c6f94d6a198b39f1d10fb51950575509b11a
Nikita Kotvitskiy <nikita.kotvitski@gmail.com>
2024-02-29 11:35:55 +0100

    fix: Periodic activity has Activity interface

--- ICS/Entities/PeriodicActivityEntity.cs
+++ ICS/Entities/PeriodicActivityEntity.cs
@@ -1,6 +1,16 @@
 ﻿namespace ICS.DAL.Entities;
-public record PeriodicActivityEntity
+public record PeriodicActivityEntity : IActivity
 {
-    public required Guid ActivityId { get; set; }
     public required DayOfWeek Day { get; set; }
+
+    public Guid EntityId { get; set; }
+    public string SubjectAbbr { get; set; } = string.Empty;
+    public string ActivityName { get; set; } = string.Empty;
+    public string TeacherLogin { get; set; } = string.Empty;
+    public int Capacity { get; set; }
+    public int Registered { get; set; }
+    public string RoomName { get; set; } = string.Empty;
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public string Description { get; set; } = string.Empty;
 }
