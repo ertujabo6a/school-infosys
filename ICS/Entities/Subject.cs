16adfe332518ca8560a236ee6b3ff8927d07fdb1
tykatsheesh <tykatuwu@gmail.com>
2024-03-14 13:51:30 +0100

    -reworked subject entity

--- ICS/Entities/Subject.cs
+++ ICS/Entities/Subject.cs
@@ -1,21 +1,11 @@
 namespace ICS.DAL.Entities;
-public record Subject
+public record Subject : IEntity
 {
-    public required string Abbr { get; set; } = string.Empty;
-    
+    public required Guid Id { get; set; }
     public required string Name { get; set; }
-    
-    public required string Description { get; set; }
-    
-    public int Year { get; set; }
-    
-    public int Points { get; set; }
-    
-    public bool Compulsorily { get; set; }
-
-
+    public required string Abbr { get; set; }
+    public required int Credits { get; set; }
     public ICollection<ActivityEntity> Activities { get; set; } = new List<ActivityEntity>();
     public ICollection<Student> Students { get; set; } = new List<Student>();
-    public ICollection<Teacher> Teachers { get; set; } = new List<Teacher>();
     
 }
