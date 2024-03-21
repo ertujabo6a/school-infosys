c0d1092028a6b27aa19de0e236dbef74c1515d1d
xbonda06 <xbonda06@vutbr.cz>
2024-03-21 15:21:22 +0100

    feat: IRepository interface implemented, added contructor to Repository class

---
+++ Source/ICS.DAL/Repositories/Repository.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICS.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ICS.DAL.Repositories;
+public class Repository<TEntity> where TEntity : class, IEntity
+{
+    private readonly DbSet<TEntity> _entities;
+    private readonly IEntityMapper<TEntity> _mapper; // TODO: integrate mappers, as they be implemented
+
+    protected Repository(DbSet<TEntity> entities, IEntityMapper<TEntity> mapper)
+    {
+        _entities = entities;
+        _mapper = mapper;
+    }
+}
