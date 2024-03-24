11ba2d39e0982e0157105e6df87317ed6e2f14bb
Aleksandr Dmitriev <dmitrievaleksandr2001@gmail.com>
2024-03-24 02:51:05 +0100

    feat: add unit of work to the project

---
+++ Source/ICS.DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using ICS.DAL.Entities;
+using ICS.DAL.Mappers;
+using ICS.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace ICS.DAL.UnitOfWork
+{
+    public sealed class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public IRepository<TEntity> GetRepository<TEntity, TEntityMapper>()
+            where TEntity : class, IEntity
+            where TEntityMapper : IEntityMapper<TEntity>, new()
+        {
+            return new Repository<TEntity>(_dbContext, new TEntityMapper());
+        }
+
+        public async Task CommitAsync()
+        {
+            await _dbContext.SaveChangesAsync().ConfigureAwait(false);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _dbContext.DisposeAsync().ConfigureAwait(false);
+        }
+    }
+}
+
---
+++ Source/ICS.DAL/UnitOfWork/UnitOfWorkFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ICS.DAL.UnitOfWork
+{
+    public class UnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        private readonly IDbContextFactory<ICSDbContext> _dbContextFactory;
+
+        public UnitOfWorkFactory(IDbContextFactory<IcsDbContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory ?? throw new ArgumentNullException(nameof(dbContextFactory));
+        }
+
+        public IUnitOfWork Create()
+        {
+            return new UnitOfWork(_dbContextFactory.CreateDbContext());
+        }
+    }
+}
