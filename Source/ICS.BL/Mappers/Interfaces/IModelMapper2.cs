0b91ac5638e974c0ddde17f6b6087b4bef527fc0
Artem Panchenko <xpanch01@vutbr.cz>
2024-03-31 20:37:39 +0200

    fix: IModelMapper added

---
+++ Source/ICS.BL/Mappers/Interfaces/IModelMapper2.cs
@@ -0,0 +1,12 @@
+﻿namespace ICS.BL.Mappers.Interfaces;
+
+public interface IModelMapper2<TEntity, TListModel>
+{
+    TListModel MapToListModel(TEntity? entity);
+
+    //Maps a collectioon of Entities to a collection of ListModels
+    IEnumerable<TListModel> MapToListModel(IEnumerable<TEntity> entities)
+        => entities.Select(MapToListModel);
+
+    TEntity MapToEntity(TListModel list_model);
+}
