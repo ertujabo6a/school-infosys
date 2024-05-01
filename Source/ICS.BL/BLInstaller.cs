fb6ae60182054a3bd344c9c66ff2574a0d29fc3e
Nikita Kotvitskiy <xkotvi01@vutbr.cz>
2024-05-01 13:31:23 +0200

    feat: BLInstaller was added

---
+++ Source/ICS.BL/BLInstaller.cs
@@ -0,0 +1,28 @@
+﻿using ICS.BL.Facades.Interfaces;
+using ICS.BL.Mappers.Interfaces;
+using ICS.DAL.UnitOfWork;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ICS.BL;
+
+public static class BLInstaller
+{
+    public static IServiceCollection AddBLService(this IServiceCollection services)
+    {
+        services.AddSingleton<IUnitOfWorkFactory, UnitOfWorkFactory>();
+
+        services.Scan(selector => selector
+            .FromAssemblyOf<BusinessLogic>()
+            .AddClasses(filter => filter.AssignableTo(typeof(IFacade<,,>)))
+            .AsMatchingInterface()
+            .WithSingletonLifetime());
+
+        services.Scan(selector => selector
+            .FromAssemblyOf<BusinessLogic>()
+            .AddClasses(filter => filter.AssignableTo(typeof(IModelMapper<,,>)))
+            .AsSelfWithInterfaces()
+            .WithSingletonLifetime());
+
+        return services;
+    }
+}
