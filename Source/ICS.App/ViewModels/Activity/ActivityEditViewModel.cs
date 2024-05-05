ae9b4405f480639f3fb76ad13f74bbe27c855301
Aleksandr Dmitriev <dmitrievaleksandr2001@gmail.com>
2024-05-05 02:57:11 +0200

    feat: add ViewModels and Messages for Activity entity

---
+++ Source/ICS.App/ViewModels/Activity/ActivityEditViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using ICS.App.Messages;
+using ICS.App.Services;
+using ICS.BL.Facades.Interfaces;
+using ICS.BL.Models;
+
+namespace ICS.App.ViewModels.Activity
+{
+    [QueryProperty(nameof(Ingredient), nameof(Ingredient))]
+    public partial class ActivityEditViewModel(
+        IActivityFacade activityFacade,
+        INavigationService navigationService,
+        IMessengerService messengerService)
+        : ViewModelBase(messengerService)
+    {
+        private readonly IMessengerService _messengerService = messengerService;
+
+        public ActivityDetailModel Ingredient { get; init; } = ActivityDetailModel.Empty;
+
+        [RelayCommand]
+        private async Task SaveAsync()
+        {
+            await activityFacade.SaveAsync(Ingredient);
+
+            _messengerService.Send(new ActivityEditMessage { ActivityId = Ingredient.Id });
+
+            navigationService.SendBackButtonPressed();
+        }
+    }
+}
