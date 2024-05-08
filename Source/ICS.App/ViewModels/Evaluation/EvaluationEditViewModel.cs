1664ddd97b6a8c8307d8e082e5d65d87d2eb6c76
Nikita Kotvitskiy <xkotvi01@vutbr.cz>
2024-05-08 14:08:37 +0200

    fix: evaluation files were restored

---
+++ Source/ICS.App/ViewModels/Evaluation/EvaluationEditViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using ICS.App.Services;
+using ICS.BL.Models;
+using ICS.BL.Facades.Interfaces;
+using ICS.App.Messages;
+
+
+namespace ICS.App.ViewModels.Evaluation;
+
+[QueryProperty(nameof(Evaluation), nameof(Evaluation))]
+public partial class EvaluationEditViewModel(
+    IEvaluationFacade evaluationFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService)
+    : ViewModelBase(messengerService)
+{
+    public EvaluationDetailModel Evaluation { get; init; } = EvaluationDetailModel.Empty;
+
+    [RelayCommand]
+    private async Task SaveAsync()
+    {
+        await evaluationFacade.SaveAsync(Evaluation);
+        MessengerService.Send(new EvaluationEditMessage { EvaluationId = Evaluation.Id });
+        navigationService.SendBackButtonPressed();
+    }
+}
