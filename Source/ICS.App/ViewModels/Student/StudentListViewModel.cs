72fef5cc4f49e03d24629ce961e2941d5f60ef31
xbonda06 <mediabondar@gmail.com>
2024-05-08 12:51:11 +0200

    feat: add StudentViewModels and Messages

---
+++ Source/ICS.App/ViewModels/Student/StudentListViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using ICS.App.Services;
+using ICS.BL.Models;
+using ICS.BL.Facades.Interfaces;
+using ICS.App.Messages;
+
+namespace ICS.App.ViewModels.Student;
+
+public partial class StudentListViewModel(
+    IStudentFacade studentFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService)
+    : ViewModelBase(messengerService), IRecipient<StudentEditMessage>, IRecipient<StudentDeleteMessage>
+{
+    public IEnumerable<StudentListModel> Students { get; set; } = null!;
+
+    protected override async Task LoadDataAsync()
+    {
+        await base.LoadDataAsync();
+
+        Students = await studentFacade.GetAsync();
+    }
+
+    [RelayCommand]
+    private async Task GoToCreateAsync()
+    {
+        await navigationService.GoToAsync("/edit");
+    }
+
+    [RelayCommand]
+    private async Task GoToDetailAsync(Guid id)
+    {
+        await navigationService.GoToAsync<StudentDetailViewModel>(
+            new Dictionary<string, object?> { [nameof(StudentDetailViewModel.Id)] = id });
+    }
+    public async void Receive(StudentEditMessage message)
+    {
+        await LoadDataAsync();
+    }
+
+    public async void Receive(StudentDeleteMessage message)
+    {
+        await LoadDataAsync();
+    }
+}
