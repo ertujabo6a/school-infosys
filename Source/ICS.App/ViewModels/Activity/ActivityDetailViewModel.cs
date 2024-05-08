a18840c9799b402853e370617ae7b66e14534dc8
Artem Panchenko <xpanch01@vutbr.cz>
2024-05-08 15:30:01 +0200

    feat: Subject View Models and messages added

---
+++ Source/ICS.App/ViewModels/Activity/ActivityDetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿/*
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using ICS.App.Messages;
+using ICS.App.Services;
+using ICS.BL.Facades;
+using ICS.BL.Models;
+
+namespace ICS.App.ViewModels.Activity;
+
+public partial class ActivityDetailViewModel(
+    ActivityFacade activityFacade,
+    )
+{
+
+}
+*/
---
+++ Source/ICS.App/ViewModels/Subject/SubjectListViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using ICS.App.Services;
+using ICS.BL.Models;
+using ICS.BL.Facades.Interfaces;
+using ICS.App.Messages;
+
+namespace ICS.App.ViewModels.Subject;
+
+public partial class SubjectListViewModel(
+    ISubjectFacade subjectFacade,
+    INavigationService navigationService,
+    IMessengerService messengerService)
+    : ViewModelBase(messengerService), IRecipient<SubjectEditMessage>, IRecipient<SubjectDeleteMessage>
+{
+
+    public IEnumerable<SubjectListModel> Subjects { get; set; } = null!;
+
+    protected override async Task LoadDataAsync()
+    {
+        await base.LoadDataAsync();
+
+        Subjects = await subjectFacade.GetAsync();
+    }
+
+    [RelayCommand]
+    private async Task GoToCreateAsync()
+    {
+        await navigationService.GoToAsync("/edit");
+    }
+
+    [RelayCommand]
+    private async Task GoToDetailAsync(Guid id)
+    {
+        await navigationService.GoToAsync<SubjectDetailViewModel>(
+            new Dictionary<string, object?> { [nameof(SubjectDetailViewModel.Id)] = id });
+    }
+
+    public async void Receive(SubjectEditMessage message)
+    {
+        await LoadDataAsync();
+    }
+
+    public async void Receive(SubjectDeleteMessage message)
+    {
+        await LoadDataAsync();
+    }
+}
