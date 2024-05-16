970d779703e28d390894599753d051a10388d199
Martin Ondrášek <tykatuwu@gmail.com>
2024-05-16 14:07:40 +0200

    feat: evaluation-views structure

---
+++ ICS.App/Views/Evaluation/EvaluationEditView.xaml.cs
@@ -0,0 +1,13 @@
+using ICS.App.ViewModels;
+
+namespace ICS.App.Views.Evaluation;
+
+public partial class EvaluationEditView
+{
+    public EvaluationEditView(EvaluationEditViewModel viewModel)
+        : base(viewModel)
+    {
+        InitializeComponent();
+    }
+}
+
