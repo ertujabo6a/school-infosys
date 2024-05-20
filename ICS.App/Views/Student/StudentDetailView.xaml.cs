5cbc4283f87fa4fc7b5d43403f725dcbc1207de5
xbonda06 <mediabondar@gmail.com>
2024-05-20 16:24:10 +0200

    feat: StudentDetailView add

---
+++ ICS.App/Views/Student/StudentDetailView.xaml.cs
@@ -0,0 +1,12 @@
+using ICS.App.ViewModels;
+
+namespace ICS.App.Views.Student;
+
+public partial class StudentDetailView
+{
+    public StudentDetailView(StudentDetailViewModel viewModel)
+        : base(viewModel)
+    {
+        InitializeComponent();
+    }
+}
